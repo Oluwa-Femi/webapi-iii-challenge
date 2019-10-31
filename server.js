70652e4ff3699d517effd6d939ac5c98642d787f
Oluwa-Femi <oluwatolablessing@gmail.com>
2019-10-31 09:08:02 +0000

    MVP done

--- server.js
+++ server.js
@@ -7,18 +7,25 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use(logger);
 server.use(cors());
+server.use("/api/users", userRouter);
 
-server.use("/api/users",userRouter);
-
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+sserver.get("/", (req, res) => {
+  res.send(`<h2>Middlewares!</h2>`);
 });
+
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      "Origin"
+    )}`
+  );
+
+  next();
+}
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
 
 module.exports = server;
